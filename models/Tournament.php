25ea6657e47402a563937e7b7424b80918f29162
Simon Nyblom <simnyb01@easv365.dk>
2025-11-24 12:40:26 +0100

    test: yml

--- models/Tournament.php
+++ models/Tournament.php
@@ -1,29 +1,34 @@
 <?php
-class Tournament {
+class Tournament
+{
     private $pdo;
 
-    public function __construct($pdo) {
+    public function __construct($pdo)
+    {
         $this->pdo = $pdo;
     }
 
-    public function getAll() {
+    public function getAll()
+    {
         $stmt = $this->pdo->query("SELECT * FROM Tournament ORDER BY startDate ASC");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function create($data) {
+    public function create($data)
+    {
         $stmt = $this->pdo->prepare("INSERT INTO Tournament (gameID, tournamentName, startDate, endDate, tournamentDescription) VALUES (?, ?, ?, ?, ?)");
         return $stmt->execute([$data['gameID'], $data['tournamentName'], $data['startDate'], $data['endDate'], $data['tournamentDescription']]);
     }
-
-    public function update($id, $data) {
+    //random comment to trigger yml
+    public function update($id, $data)
+    {
         $stmt = $this->pdo->prepare("UPDATE Tournament SET gameID=?, tournamentName=?, startDate=?, endDate=?, tournamentDescription=? WHERE tournamentID=?");
         return $stmt->execute([$data['gameID'], $data['tournamentName'], $data['startDate'], $data['endDate'], $data['tournamentDescription'], $id]);
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $stmt = $this->pdo->prepare("DELETE FROM Tournament WHERE tournamentID=?");
         return $stmt->execute([$id]);
     }
 }
-?>
