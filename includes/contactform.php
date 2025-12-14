c58389b09548fd5774525ca80999645074cf33ba
Simon Nyblom <simnyb01@easv365.dk>
2025-12-14 13:56:51 +0100

    fix: naming

--- includes/contactform.php
+++ includes/contactform.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $firstName = trim($_POST['firstName']);
     $lastName = trim($_POST['lastName']);
     $email = trim($_POST['email']);
-    $category = $_POST['support'];
+    $category = $_POST['category'];
     $message = trim($_POST['message']);
     $tournamentID = !empty($_POST['tournament']) ? intval($_POST['tournament']) : null;
 
