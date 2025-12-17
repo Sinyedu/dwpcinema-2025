60fff562bb669255f0eb801b908cd621230d5138
Simon Nyblom <simnyb01@easv365.dk>
2025-12-17 02:04:41 +0100

    fix: updated showing controller

--- controllers/ShowingController.php
+++ controllers/ShowingController.php
@@ -1,26 +1,32 @@
 <?php
-class ShowingController {
+class ShowingController
+{
     private $showingModel;
     private $bookingModel;
 
-    public function __construct(Showing $showingModel, Booking $bookingModel) {
+    public function __construct(Showing $showingModel, Booking $bookingModel)
+    {
         $this->showingModel = $showingModel;
         $this->bookingModel = $bookingModel;
     }
 
-    public function listShowings($tournamentID = null) {
+    public function listShowings($tournamentID = null)
+    {
         return $this->showingModel->getAll($tournamentID);
     }
 
-    public function showDetails($showingID) {
+    public function showDetails($showingID)
+    {
         return $this->showingModel->getById($showingID);
     }
 
-    public function bookShowing($userID, $showingID) {
-        return $this->bookingModel->create($userID, $showingID);
+    public function bookShowing($userID, $showingID, array $seatIDs)
+    {
+        return $this->bookingModel->create($userID, $showingID, $seatIDs);
     }
 
-    public function userBookings($userID) {
+    public function userBookings($userID)
+    {
         return $this->bookingModel->getByUser($userID);
     }
 }
